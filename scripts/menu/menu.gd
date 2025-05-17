8e7bbf85cbc3a22be3602a184530a14c051b25cd
loyerg <gavril.loyer@grenoble-inp.org>
2025-05-17 15:29:43 -0300

    Move of files to correspond to the pattern of the main

---
+++ scripts/menu/menu.gd
@@ -0,0 +1,31 @@
+extends Control
+@onready var menu_select = preload("res://scenes/menu/shop.tscn")
+@onready var credits = preload("res://scenes/menu/credits.tscn")
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:
+	pass
+
+
+func _on_button_start() -> void:
+	# Bouton start
+	print("Start is pressed")
+	get_tree().change_scene_to_packed(menu_select)
+	
+
+
+func _on_button_quit() -> void:
+	# Bouton quitter
+	print("Quit is pressed")
+	get_tree().quit()
+
+
+func _on_button_credits() -> void:
+	# Bouton crédits
+	print("Credits is pressed")
+	get_tree().change_scene_to_packed(credits)
