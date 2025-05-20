2a11c3d5de6f10e024a6752dea03e19be3e028ce
loyerg <gavril.loyer@grenoble-inp.org>
2025-05-20 20:42:55 -0300

    Pr review Correction

--- scripts/menu/menu.gd
+++ scripts/menu/menu.gd
@@ -2,30 +2,18 @@ extends Control
 @onready var menu_select = preload("res://scenes/menu/shop.tscn")
 @onready var credits = preload("res://scenes/menu/credits.tscn")
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta: float) -> void:
-	pass
-
-
 func _on_button_start() -> void:
-	# Bouton start
+	# Start Button
 	print("Start is pressed")
 	get_tree().change_scene_to_packed(menu_select)
-	
-
 
 func _on_button_quit() -> void:
-	# Bouton quitter
+	# Quit Button 
 	print("Quit is pressed")
 	get_tree().quit()
 
 
 func _on_button_credits() -> void:
-	# Bouton crédits
+	# Credits Bouton 
 	print("Credits is pressed")
 	get_tree().change_scene_to_packed(credits)
