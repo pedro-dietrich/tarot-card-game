1fdce4708a085104de423102d13631280ab2d3b1
Yusei234 <pit.langelot@gmail.com>
2025-06-22 21:35:34 -0300

    assets for the elemental card

--- scripts/cards/elementals/wind.gd
+++ scripts/cards/elementals/wind.gd
@@ -1,5 +1,27 @@
 class_name Wind extends Element
 
+var base_card_image_path = "res://assets/card/air_png/"
+
+var card_image = [
+	"a1.png",
+	"a2.png",
+	"a3.png",
+	"a4.png",
+	"a5,png",
+	"a6.png",
+	"a7.png",
+	"a8.png",
+	"a9.png",
+	"a10.png",
+	"a11.png",
+	"a12.png",
+	"a13.png",
+	"a14.png"
+]
+
+func get_image_path() -> String:
+	return base_card_image_path + card_image[id - 1]
+
 func get_points(played_cards: Array[ElementalCard]) -> float:
 	var wind_card_count: int = 0
 	for card in played_cards:
