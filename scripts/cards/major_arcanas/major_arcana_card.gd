e8a4accf54cd09c7a2c8030969e8c541ef0766b1
Yusei234 <pit.langelot@gmail.com>
2025-06-24 19:40:46 -0300

    Bug corretion

--- scripts/cards/major_arcanas/major_arcana_card.gd
+++ scripts/cards/major_arcanas/major_arcana_card.gd
@@ -7,8 +7,11 @@ func set_major_arcana(major_arcana_var):
 	$CardLabel.text = major_arcana_var.card_name
 	
 func set_card_images() -> void:
-	var material: Material = card_mesh.get_active_material(0)
+	var material: Material = ShaderMaterial.new()
+	material.shader = load("res://shaders/card.gdshader")
 	if(material is ShaderMaterial):
 		material.set_shader_parameter("background", load(major_arcana.get_background()))
 		material.set_shader_parameter("middleground", load(major_arcana.get_middleground()))
 		material.set_shader_parameter("foreground", load(major_arcana.get_foreground()))
+		material.set_shader_parameter("base_albedo", load("res://assets/card/card.jpg"))
+	$Card.material_override = material
