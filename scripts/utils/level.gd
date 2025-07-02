bca663bc610bcd979f2f8543527535db489770a9
Gavril Loyer <gavril.loyer@grenoble-inp.org>
2025-07-01 21:49:17 -0300

    Add of a line that shouldn't have been removed

--- scripts/utils/level.gd
+++ scripts/utils/level.gd
@@ -53,6 +53,7 @@ func increment() -> void:
 
 func get_card_points(played_card: ElementalCard, played_cards, bonus_arcanas):
 	# Change the value of the card depending on the major arcana rule
+	played_card.point = get_malus_arcana().malus_effect_on_points(played_cards, LVL_MAX_CARDS_PLAYED[level])
 	for major_bonus in bonus_arcanas:
 		played_card.point = major_bonus.major_arcana.bonus_effect_on_points(played_cards, LVL_MAX_CARDS_PLAYED[level])
 	
